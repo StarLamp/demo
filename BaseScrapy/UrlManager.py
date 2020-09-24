b7d6a4dd7162d26200477b89a25980cbc000c1ea
StarLamp <875652194@qq.com>
2020-09-24 15:44:54 +0800

    version1.0

--- BaseScrapy/UrlManager.py
+++ BaseScrapy/UrlManager.py
@@ -11,4 +11,23 @@ class UrlManager(object):
         self.old_urls.add(new_url)
         return new_url
     def add_new_url(self,url):
-        #将
+        #将新的URL添加到为爬取的URL集合中
+        #：param url:单个URL
+        #：return:
+        if url is None:
+            return
+        if url not in self.new_urls and url not in self.old_urls:
+            self.new_urls.add(url)
+
+    def add_new_urls(self,urls):
+        #将新的URL添加到未爬取的URL集合中
+        if urls is None or len(urls)==0:
+            return
+        for url in urls:
+            self.add_new_url(url)
+    def new_url_size(self):
+        #获取未爬取URL集合的大小
+        return len(self.new_urls)
+
+    def old_url_siez(self):
+        return len(self.old_urls)
