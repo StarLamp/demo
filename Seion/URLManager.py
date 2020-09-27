0d231681a328c6674c01056c28bd1e0d74b3ca78
StarLamp <875652194@qq.com>
2020-09-27 17:27:45 +0800

    version1.0

--- Seion/URLManager.py
+++ Seion/URLManager.py
@@ -25,6 +25,31 @@ class UrlManager(object):
         self.old_urls.add(m.hexdigest()[8:-8])
         return new_url
 
+    def add_new_url(self,url):
+        '''
+        将新的URL添加到未爬取的
+        :param url:
+        :return:
+        '''
+        if url is None:
+            return
+        m = hashlib.md5()
+        m.update(url)
+        url_md5 = m.hexdigest()[8:-8]
+        if url not in self.new_urls and url_md5 not  in self.old_urls:
+            self.new_urls.add(url)
+
+    def add_new_urls(self,urls):
+        '''
+        将新的URL添加到未爬取的URl集合中
+        :param urls:
+        :return:
+        '''
+        if urls is None or len(urls)==0:
+            return
+        for url in urls:
+            self.add_new_url(url)
+
 
     def load_process(self, param):
         pass
